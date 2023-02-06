d9ed765d33324a880f3ad83a29fab02ad02e2e99
2_Lock_Tsun <69589581+2LockTsun@users.noreply.github.com>
2023-02-06 14:07:56 +0000

    Random commits

--- src/front-end/app/page.js
+++ src/front-end/app/page.js
@@ -5,5 +5,17 @@ import styles from './page.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-	return <main className={styles.main}>Hello World!</main>;
+	return <main className={styles.main}>
+		<nav>
+			<a herf={`https://www.google.com`}>
+				<div style={styles.Home}>
+					<h1>Home</h1>
+				</div>
+			</a>
+			<div>Test</div>
+		</nav>
+		<div>
+			Hello World!
+		</div>
+	</main>;
 }
