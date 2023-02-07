9a05be5702fd0930b8295fbbfb37907a6046d7f7
2locktsun <wonkalok5@outlook.com>
2023-02-07 14:00:57 +0800

    Update frontend - Create Sample page

--- src/front-end/app/page.jsx
+++ src/front-end/app/page.jsx
@@ -7,12 +7,17 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Main() {
 	return <main className={styles.main}>
-		<Link href={'/test'}>
+		<Link href={'/sample'}>
 			<div style={styles.Home}>
 				<h1>Test</h1>
 			</div>
 		</Link>
-		<div>Home</div>
+		<div className={styles.center}>
+			<div>
+				Welcom to Classroom Booking System!<br/>
+				Choose the function from the above.
+			</div>
+		</div>
 		<div>
 			Hello World!
 		</div>
