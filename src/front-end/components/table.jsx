cdf9cd9ddbf6e3e20749e9a4c963e106f00af35d
2locktsun <wonkalok5@outlook.com>
2023-02-13 11:13:54 +0800

    Create table component

---
+++ src/front-end/components/table.jsx
@@ -0,0 +1,11 @@
+import styles from '@/components/table.module.css'
+
+export default function Table(list) {
+    return <div>
+        <div style={styles.row}>
+            <div style={styles.column}>
+
+            </div>
+        </div>
+    </div>
+}
