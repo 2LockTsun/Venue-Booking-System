83d390ca8f3cf8a0635cf4047b6b2416acd45e1b
2locktsun <wonkalok5@outlook.com>
2023-02-13 11:15:14 +0800

    Adding table component to test the code

--- src/front-end/app/page.jsx
+++ src/front-end/app/page.jsx
@@ -2,16 +2,25 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import Table from '@/components/table';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Main() {
 	return <main className={styles.main}>
-		<Link href={'/sample'}>
-			<div style={styles.Home}>
-				<h1>Test</h1>
-			</div>
-		</Link>
+		<div>
+			<Link href={'/sample'}>
+				<div style={styles.Home}>
+					<h1>Test</h1>
+				</div>
+			</Link>
+			<Link href={'/'}>
+				<div style={styles.h1}>
+					<h1>Check booked</h1>
+				</div>
+			</Link>
+		</div>
+		<Table/>
 		<div className={styles.center}>
 			<div>
 				Welcom to Classroom Booking System!<br/>
