b4b39d22538b299eb04aeecd6554192bce66e1cb
2LockTsun <wonkalok5@outlook.com>
2023-04-14 12:56:14 +0800

    BE - update api login method

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-    if (req.method != 'GET') res.status(405).json({ message: 'Method Not Allow' })
+    if (req.method != 'POST') res.status(405).json({ message: 'Method Not Allow' })
     res.status(200).json({ message: 'OK' })
 }
