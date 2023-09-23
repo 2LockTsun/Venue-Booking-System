bf2c3075bcb57641c85d502b91d155140304adac
2LockTsun <wonkalok5@outlook.com>
2023-09-23 11:00:45 +0800

    giving more record as ref

--- src/prisma/seed.js
+++ src/prisma/seed.js
@@ -50,6 +50,162 @@ async function main() {
         update: {},
         create: {roomCode: 'LT', roomName: 'Lecture Theatre'},
     })
+    await prisma.venue.upsert({
+        where: {roomCode: 'IS',},
+        update: {},
+        create: {roomCode: 'IS', roomName: 'IS Lab'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: 'P',},
+        update: {},
+        create: {roomCode: 'P', roomName: 'Physics Lab'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: 'C',},
+        update: {},
+        create: {roomCode: 'C', roomName: 'Chemistry Lab'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: 'B',},
+        update: {},
+        create: {roomCode: 'B', roomName: 'Biology Lab'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: 'H',},
+        update: {},
+        create: {roomCode: 'H', roomName: 'Hall'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: 'DT',},
+        update: {},
+        create: {roomCode: 'DT', roomName: 'DT Room'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: 'FT',},
+        update: {},
+        create: {roomCode: 'FT', roomName: 'Fitness Room'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '1A',},
+        update: {},
+        create: {roomCode: '1A', roomName: '1A'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '1B',},
+        update: {},
+        create: {roomCode: '1B', roomName: '1B'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '1C',},
+        update: {},
+        create: {roomCode: '1C', roomName: '1C'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '1D',},
+        update: {},
+        create: {roomCode: '1D', roomName: '1D'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '2A',},
+        update: {},
+        create: {roomCode: '2A', roomName: '2A'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '2B',},
+        update: {},
+        create: {roomCode: '2B', roomName: '2B'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '2C',},
+        update: {},
+        create: {roomCode: '2C', roomName: '2C'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '2D',},
+        update: {},
+        create: {roomCode: '2D', roomName: '2D'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '3A',},
+        update: {},
+        create: {roomCode: '3A', roomName: '3A'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '3B',},
+        update: {},
+        create: {roomCode: '3B', roomName: '3B'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '3C',},
+        update: {},
+        create: {roomCode: '3C', roomName: '3C'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '3D',},
+        update: {},
+        create: {roomCode: '3D', roomName: '3D'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '4A',},
+        update: {},
+        create: {roomCode: '4A', roomName: '4A'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '4B',},
+        update: {},
+        create: {roomCode: '4B', roomName: '4B'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '4C',},
+        update: {},
+        create: {roomCode: '4C', roomName: '4C'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '4D',},
+        update: {},
+        create: {roomCode: '4D', roomName: '4D'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '5A',},
+        update: {},
+        create: {roomCode: '5A', roomName: '5A'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '5B',},
+        update: {},
+        create: {roomCode: '5B', roomName: '5B'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '5C',},
+        update: {},
+        create: {roomCode: '5C', roomName: '5C'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '5D',},
+        update: {},
+        create: {roomCode: '5D', roomName: '5D'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '6A',},
+        update: {},
+        create: {roomCode: '6A', roomName: '6A'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '6B',},
+        update: {},
+        create: {roomCode: '6B', roomName: '6B'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '6C',},
+        update: {},
+        create: {roomCode: '6C', roomName: '6C'},
+    })
+    await prisma.venue.upsert({
+        where: {roomCode: '6D',},
+        update: {},
+        create: {roomCode: '6D', roomName: '6D'},
+    })
+    
 
 
     await prisma.staff.upsert({
@@ -94,6 +250,62 @@ async function main() {
             email: 'ya@cpu.edu.hk',
             password: 'ya',
             staffInitial: 'YA',
+        }
+    })
+
+    await prisma.staff.upsert({
+        where: { staffInitial: 'T1' },
+        update: {},
+        create: {
+            admin: false,
+            name: 'Test 1',
+            email: 'test1@test.com',
+            password: '123456',
+            staffInitial: 'T1',
+        }
+    })
+    await prisma.staff.upsert({
+        where: { staffInitial: 'T2' },
+        update: {},
+        create: {
+            admin: false,
+            name: 'Test 2',
+            email: 'test2@test.com',
+            password: '123456',
+            staffInitial: 'T2',
+        }
+    })
+    await prisma.staff.upsert({
+        where: { staffInitial: 'T3' },
+        update: {},
+        create: {
+            admin: false,
+            name: 'Test 3',
+            email: 'test3@test.com',
+            password: '123456',
+            staffInitial: 'T3',
+        }
+    })
+    await prisma.staff.upsert({
+        where: { staffInitial: 'T4' },
+        update: {},
+        create: {
+            admin: false,
+            name: 'Test 4',
+            email: 'test4@test.com',
+            password: '123456',
+            staffInitial: 'T4',
+        }
+    })
+    await prisma.staff.upsert({
+        where: { staffInitial: 'T5' },
+        update: {},
+        create: {
+            admin: false,
+            name: 'Test 5',
+            email: 'test5@test.com',
+            password: '123456',
+            staffInitial: 'T5',
         }
     })
 }
